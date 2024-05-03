8a07ca3d0496a166ff989d0786d2bfd80c5b7c40
zzeini <111908318+zaynabzeini@users.noreply.github.com>
2024-05-03 13:47:22 -0500

    push !

--- Q3.cpp
+++ Q3.cpp
@@ -17,57 +17,64 @@ mutex mtx;
 condition_variable cv;
 queue<requestStructure> msg_queue;
 int counter = 0;
-int nListeners = 0;
+int numThreads = 4;
+bool listenExit = false;
+string webPages[10] = {"google.com", "yahoo.com", "gmail.com", "canvas.com", "submittable.com", "instagram.com", "twitter.com", "paliroots.com", "neworleansreview.com", "massreview.com",};
 
-void listen(string webPages[]) {
+
+void listen() {
     srand(time(NULL));
+    requestStructure request; // generate a request with
     for (int i = 1; i <= 10; ++i) {
         int time = rand() % 3 + 1;
         int j = rand() % 10 + 0;
         this_thread::sleep_for(chrono::seconds(time)); // sleep for random number between 1-3 seconds using sleep_for instruction
         {   
-            requestStructure request; // generate a request with
-            counter++;
-            request.request_id = counter; // request_id from a counter shared
+            request.request_id = i; // request_id from a counter shared
             request.ip_address = ""; // ip_address as an empty string
             request.page_requested = webPages[j]; // page_request as a random web page name from the array webPages defined above
             lock_guard< mutex> lock(mtx);
             msg_queue.push(request); // save the request in msg_queue
+            cout << "listener has generated request " << i << endl;
         }
         cv.notify_one(); // notify one thread to consume it
     }
+    listenExit = true;
+    cv.notify_all(); // wake up all the other threads that are waiting for cv
 }
 
 void do_request(int thread_id) {
     while (true) {
         unique_lock< mutex> lock(mtx);
 
-        // if the msg_queue is empty, then wait using conditional variable
+        // if the msg_queue is empty, then wait using conditional variable // once it's notified, it will wake up the thread
         while (msg_queue.empty()) {
-            cv.wait(lock);
+            if (listenExit == true) {
+                cout << "thread " << thread_id << " exits" << endl;
+                return; // this means that there are no more requests to do, the listening is done
+            }
+            cv.wait(lock); // wait will release the lock, the htread that's waiting will wake up and move to the ready queue and hold the lock
         }
 
         // else get the request from the msg_queue
         requestStructure request = msg_queue.front();  // int message = msg_queue.front();
         msg_queue.pop(); // msg_queue.pop();
-        lock.unlock(); // lock.unlock();
         cout << "Thread " << thread_id << " completed request " << request.request_id << " requesting webpage " << request.page_requested << endl;
+        lock.unlock(); // lock.unlock();
 
     }
 }
 
 int main() {
-    string webPages[10] = {"google.com", "yahoo.com", "gmail.com", "canvas.com", "submittable.com", "instagram.com", "twitter.com", "paliroots.com", "neworleansreview.com", "massreview.com",};
-    thread listeners[nListeners]; // array of threads that search for the requested web pages    
-    thread reciever(listen, webPages);
-
-    for (int k = 0; k < nListeners; k++) {
-        listeners[k] = thread(do_request, k); // thread that search for the  requested pages and send them to the client 
-        reciever.join();
-        listeners[k].join();
+    thread listener(listen);
+    thread recievers[numThreads];
+    for (int i = 0; i < numThreads; i++) {
+        recievers[i] = thread(do_request, i);
     }
-
-    cout << "Main thread finished" << endl;
+    listener.join();
+    for (int i = 0; i < numThreads; i++) {
+        recievers[i].join();
+    }
 
     return 0;
 }
